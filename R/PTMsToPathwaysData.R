4a406f34251730fb948d4f658f2b3e28c0a9dff8
Lucy <lgw2@uw.edu>
2026-06-30 23:26:25 -0600

    add placeholder vignettes and man pages

--- R/PTMsToPathwaysData.R
+++ R/PTMsToPathwaysData.R
@@ -2,7 +2,7 @@
 #'
 #' Returns ExperimentHub records tagged for PTMsToPathways BRCA data.
 #'
-#' @return A subsetted \\code{ExperimentHub} object.
+#' @return A subsetted \code{ExperimentHub} object.
 #' @export
 PTMsToPathwaysData <- function() {
   eh <- ExperimentHub::ExperimentHub()
